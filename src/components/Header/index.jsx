58c2b92477ab4f52c57f501d2d74fdea51ae00ed
Sudharsan RaviKumar <supersudh@gmail.com>
2023-08-23 14:36:16 +0530

    Add search bar to header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,39 +1,53 @@
 import Image from "next/image";
 import Stack from '@mui/material/Stack';
+import { Hidden } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
 
 import styles from './Header.module.css';
-import { Hidden } from "@mui/material";
+
 
 export default function Header() {
   return (
-    <div>
+    <Stack Stack alignItems="center" direction="row" justifyContent="space-between" className={styles.stackContainer} >
       {/* Left */}
+      <Hidden lgDown>
+        <div className={styles.heroImageContainer}>
+          <Image
+            src="https://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
+            layout="fill"
+            className={styles.heroImage}
+          />
+        </div>
+      </Hidden>
+      <Hidden lgUp>
+        <div className={styles.heroImageContainerMobile}>
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+            layout="fill"
+            className={styles.heroImage}
+          />
+        </div>
+      </Hidden>
 
-      <Stack alignItems="center" flexDirection="row" justifyContent="space-between" className={styles.stackContainer}>
-        <Hidden lgDown>
-          <div className={styles.heroImageContainer}>
-            <Image
-              src="https://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
-              layout="fill"
-              className={styles.heroImage}
-            />
-          </div>
-        </Hidden>
-        <Hidden lgUp>
-          <div className={styles.heroImageContainerMobile}>
-            <Image
-              src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
-              layout="fill"
-              className={styles.heroImage}
-            />
-          </div>
-        </Hidden>
+      {/* Middle */}
+      <div className="relative">
+        <div className="absolute">
+          <IconButton sx={{ p: '10px' }} aria-label="search" className={styles.searchIconContainer}>
+            <SearchIcon className="text-gray-500" />
+          </IconButton>
+        </div>
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search"
+            inputProps={{ 'aria-label': 'search' }}
+            className={`${styles.searchInput} text-sm`}
+          />
+      </div>
 
-        {/* Middle */}
-
-        {/* Right */}
-        <div>Right Sides</div>
-      </Stack>
-    </div>
+      {/* Right */}
+      <div>Right Sides</div>
+    </Stack>
   );
 }

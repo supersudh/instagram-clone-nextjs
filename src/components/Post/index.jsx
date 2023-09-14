66f226f1c1449160998747b22c408e20f3837450
Sudharsan RaviKumar <supersudh@gmail.com>
2023-09-14 15:12:48 +0530

    Update Post component

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import IconButton from '@mui/material/IconButton';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Stack from '@mui/material/Stack';
+
+import styles from './Post.module.css';
 
 export default function Post({ id, username, userImage, img, caption }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className={styles.post}>
+      {/* Post Header */}
+
+      <Stack flexDirection="row" alignItems="center" padding="1.25rem">
+        <img className={styles['header-user-img']} src={userImage} alt={username} />
+        <p className="font-bold flex-1">{username}</p>
+        <IconButton className="icon">
+          <MoreHorizIcon />
+        </IconButton>
+      </Stack>
+      
+      {/* Post Image */}
+      
+      <img src={img} alt="" className={styles.postImage} />
+      
+      {/* Post Buttons */}
+      
+      <div className="">
+        <div>
+        
+        </div>
+      </div>
     </div>
   );
 }

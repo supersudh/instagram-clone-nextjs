341a2ebf3659dd9a531ea3bc7d4ec4d5142e55af
Sudharsan RaviKumar <supersudh@gmail.com>
2023-09-14 14:51:06 +0530

    Create posts and Post components and make some dummy data

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.jennexplores.com', 'upload.wikimedia.org', 'media.licdn.com']
+    domains: ['www.jennexplores.com', 'upload.wikimedia.org', 'media.licdn.com', 'images.unsplash.com']
   }
 };
 
